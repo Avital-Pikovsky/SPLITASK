4f7b4fa94614b4e4276f2f8de59ca72cbf168c51
OmerK25 <omer2katz@gmail.com>
2020-11-18 16:29:49 +0200

    fix name

--- app/src/main/java/com/example/duckluck/addNewList.java
+++ app/src/main/java/com/example/duckluck/addNewList.java
@@ -3,12 +3,25 @@ package com.example.duckluck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class addNewList extends AppCompatActivity {
+
+    private EditText listName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_new_list2);
+        setContentView(R.layout.activity_add_new_list);
+        setupUI();
+
     }
+
+    private void setupUI() {
+        listName = (EditText) findViewById(R.id.listname);
+    }
+
 }
