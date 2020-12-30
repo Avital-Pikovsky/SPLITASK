c3a269f5e2cd00baceebab42f7a5511eeb2b6ffe
Avital Pikovsky <avitali1996@gmail.com>
2020-12-30 10:50:30 +0200

    Added all admin activities

---
+++ app/src/main/java/Activities/UserJoinedListsActivity.java
@@ -0,0 +1,13 @@
+package Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserJoinedListsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_joined_lists);
+    }
+}
---
+++ app/src/main/java/Activities/UserListsActivity.java
@@ -0,0 +1,13 @@
+package Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserListsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_lists);
+    }
+}
